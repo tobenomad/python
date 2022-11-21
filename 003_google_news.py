3d1c723bbf4df52a2bbcf746e3f7f3999d022937
tobenomad <tobenomad@gmail.com>
2022-11-21 17:02:11 +0900

    2022.11.21 구글 뉴스 크롤링 max attempt 오류 발생

--- 003_google_news.py
+++ 003_google_news.py
@@ -10,4 +10,20 @@ html_src = resp.text
 
 soup = BeautifulSoup(html_src, 'html.parser')
 
-print(soup)
+# 뉴스 아이템 블록을 선택
+news_items = soup.select('div[class="XlKvRb"]')
+# print(len(news_items))
+# print(news_items[0])
+# print("\n")
+
+# 각 뉴스 아이템에서 "링크, 제목, 내용, 출처, 등록일시" 데이터를 파싱(BeautifulSoup)
+for item in news_items[:3]:
+    link = item.find('a', attrs={'class':'WwrzSb'}).get('href')
+    news_link = base_url + link[1:]
+    print(news_link)
+    
+    news_title = item.find('hr4', attrs={'class':'gPFEn'}).getText()
+    print(news_title)
+    
+    news_content = item.find('span', attrs={'class':'xBbh9'}).text
+    print(news_content)
